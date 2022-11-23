e41f4add1beb24166bfb56c58d780cee7bb0e810
DaniNasciment <dannni01@hotmail.com>
2022-11-23 12:33:33 -0300

    feat:react

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import { Title} from "./components/Text/Text"
+import { Text } from "./components/Title/Title"
+
+
+function App() {
+  const nome = 'Daniela';
+
+
+  return (
+    <> 
+      <p>
+         
+      </p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Title.css';
+ 
+function Text() {
+    return (
+        <p> Teste </p>
+    )
+}
+ 
+export {Title}
